e9a20c4f210db2dbea19741c2f8b1ef318d8da76
Olufisayo Bamidele <fisiwizy@gmail.com>
2020-12-22 16:42:37 +0100

    serve with express

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.NODE_ENV === 'production' ? 80 : 4000;
+app.use(express.static('frontend'));
+
+app.all('*', (_, res) => {
+    return res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
+// app.listen(PORT, '0.0.0.0', () => console.log('Server started'));
+app.listen(PORT, () => console.log('Server started'));
